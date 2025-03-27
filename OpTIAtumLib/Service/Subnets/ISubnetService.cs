e0a21e9fb06ab622dff39accf753b8b49d327000
amasovich <amasovich@yandex.ru>
2025-03-27 16:17:45 +0300

    Create draft ISubnetService & SubnetService

---
+++ OpTIAtumLib/Service/Subnets/ISubnetService.cs
@@ -0,0 +1,50 @@
+﻿using Siemens.Engineering;
+using Siemens.Engineering.HW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpTIAtumLib.Service.Subnets
+{
+    internal interface ISubnetService
+    {
+        /// <summary>
+        /// Создает подсеть, подключённую к узлу.
+        /// Тип подсети определяется типом интерфейса, к которому подключается узел.
+        /// </summary>
+        /// <param name="node">Объект узла (Node), например, соответствующий физическому интерфейсу устройства</param>
+        /// <param name="subnetName">Имя создаваемой подсети</param>
+        /// <returns>Созданный объект подсети</returns>
+        Subnet CreateAndConnectSubnet(Node node, string subnetName);
+
+        /// <summary>
+        /// Создает подсеть в составе проекта без привязки к узлу.
+        /// Используется идентификатор типа, например "System:Subnet.Ethernet".
+        /// </summary>
+        /// <param name="project">Открытый проект TIA Portal</param>
+        /// <param name="subnetTypeIdentifier">Идентификатор типа подсети (например, "System:Subnet.Ethernet")</param>
+        /// <param name="subnetName">Имя новой подсети</param>
+        /// <returns>Созданный объект подсети</returns>
+        Subnet CreateStandaloneSubnet(Project project, string subnetTypeIdentifier, string subnetName);
+
+        //Задача: Создать изолированную подсеть (например, PROFINET, PROFIBUS).
+        Subnet CreateStandaloneSubnet(string typeIdentifier, string name);
+
+        //Задача: Создать подсеть и сразу подключить её к Node (сетевому интерфейсу устройства/модуля).
+        Subnet CreateAndConnectToNode(Node node, string subnetName);
+
+        //Задача: Подключить DeviceItem, имеющий NetworkInterface, к уже существующей подсети.
+        void ConnectDeviceItemToSubnet(DeviceItem deviceItem, Subnet subnet);
+
+        //Задача: Найти все Node, которые можно подключать к подсетям (используется для отладки и отображения).
+        List<Node> GetAllConnectableNodes(Project project);
+
+        //Задача: Если подсеть уже существует — вернуть её.Если нет — создать.
+        Subnet EnsureOrCreateSubnet(Project project, string typeIdentifier, string subnetName);
+
+
+
+    }
+}
---
+++ OpTIAtumLib/Service/Subnets/SubnetService.cs
@@ -0,0 +1,32 @@
+﻿using Siemens.Engineering;
+using Siemens.Engineering.HW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpTIAtumLib.Service.Subnets
+{
+    internal class SubnetService //: ISubnetService
+    {
+        /// <inheritdoc/>
+        public Subnet CreateAndConnectSubnet(Node node, string subnetName)
+        {
+            // Создание и подключение подсети к узлу
+            Subnet subnet = node.CreateAndConnectToSubnet(subnetName);
+            Console.WriteLine($"Подсеть '{subnetName}' успешно создана и подключена к узлу '{node.Name}'.");
+            return subnet;
+        }
+
+        public Subnet CreateStandaloneSubnet(Project project, string subnetTypeIdentifier, string subnetName)
+        {
+            // Получаем коллекцию подсетей из проекта
+            SubnetComposition subnets = project.Subnets;
+            // Создаем новую подсеть с заданным типом
+            Subnet newSubnet = subnets.Create(subnetTypeIdentifier, subnetName);
+            Console.WriteLine($"Подсеть '{subnetName}' типа '{subnetTypeIdentifier}' успешно создана в проекте.");
+            return newSubnet;
+        }
+    }
+}
